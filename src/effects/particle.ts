0f1397bff71665e9229492df73b508d745e4df5b
72 <72sevenzy2@gmail.com>
2025-10-13 13:55:20 +0500

    updated particles, so that each particle disssapears after a give amount of time

--- src/effects/particle.ts
+++ src/effects/particle.ts
@@ -19,21 +19,22 @@ interface Particle {
     vy: number;
     radius: number;
     life: number;
-    maxLife: number;
 }
 
 export class Particles implements Effect {
     private amount: number;
     private radius: number;
     private speed: number;
+    private zlife: number;
     private clicked: boolean = false;
     private particles: Particle[] = [];
-    constructor({ amount = 100, radius = 10, speed = 10 }: {
-        amount?: number, x?: number, y?: number, radius?: number, speed?: number
+    constructor({ amount = 1000, radius = 10, speed = 10, zlife = 100 }: {
+        amount?: number, x?: number, y?: number, radius?: number, speed?: number, zlife?: number
     } = {}) {
         this.amount = amount;
         this.radius = radius;
         this.speed = speed;
+        this.zlife = zlife;
 
         window.addEventListener("mousedown", () => {
             this.clicked = true;
@@ -55,28 +56,28 @@ export class Particles implements Effect {
                     vx: (Math.random() - 0.5) * this.speed * 2,
                     vy: (Math.random() - 0.5) * this.speed * 2,
                     radius: this.radius + Math.random() * 1.5,
-                    life: Math.random() * 40 + 30,
-                    maxLife: Math.random() * 40 + 30
+                    life: this.zlife
                 });
             }
-            this.clicked = false;
         }
+        this.clicked = false;
 
-        // clearing every dead particle
-
-        this.particles = this.particles.filter((p) => p.life > 0);
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
         // drawing
-
         for (const particle of this.particles) {
             particle.x += particle.vx;
             particle.y += particle.vy;
+            particle.life -= 1;
 
-            const alpha = particle.life / particle.maxLife;
+            const alpha = Math.max(particle.life / 100, 0);
             ctx.beginPath();
             ctx.arc(particle.x, particle.y, particle.radius, 0, Math.PI * 2, false);
             ctx.fillStyle = `rgba(255, 255, 255, ${alpha})`;
             ctx.fill();
         }
+
+        this.particles = this.particles.filter(p => p.life > 0);
+
     }
 }
