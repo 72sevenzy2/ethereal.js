d9a295ac258f0ab74ab148caa5753f578a3340d5
72 <72sevenzy2@gmail.com>
2025-10-27 19:21:54 +0500

    fixed circles getting stuck inside wall: fixing 2 or more circles on fixed positions

--- main/effects/utils/2dcollisions.js
+++ main/effects/utils/2dcollisions.js
@@ -1,25 +1,18 @@
 // more enhanced 2d collision equation in code
-
-import { circleType } from "../circles";
-
-export function resolveCollisions(p1: circleType, p2: circleType) {
+export function resolveCollisions(p1, p2) {
     const d1x = p2.positionX - p1.positionX;
     const d1y = p2.positionY - p1.positionY;
     const hyp = Math.hypot(d1x, d1y);
-
     if (hyp === 0) {
-        return null
+        return null;
     }
-
     const overlap = 0.5 * (p1.radius + p2.radius - hyp);
     const sock = (d1x / hyp) * overlap;
     const rock = (d1y / hyp) * overlap;
-
     p1.positionX -= sock;
     p1.positionY -= rock;
     p2.positionX += sock;
     p2.positionY += rock;
-
     const dx = p2.positionX - p1.positionX;
     const dy = p2.positionY - p1.positionY;
     const phi = Math.atan2(dy, dx);
@@ -29,23 +22,14 @@ export function resolveCollisions(p1: circleType, p2: circleType) {
     const theta2 = Math.atan2(p2.velocityY, p2.velocityX);
     const m1 = p1.mass;
     const m2 = p2.mass;
-
-    const v1x = (
-        ((v1 * Math.cos(theta1 - phi)) * (m1 - m2) + 2 * m2 * v2 * Math.cos(theta2 - phi)) / (m1 + m2)
-    ) * Math.cos(phi) + v1 * Math.sin(theta1 - phi) * Math.cos(phi + Math.PI / 2);
-    const v1y = (
-        ((v2 * Math.cos(theta1 - phi)) * (m1 - m2) + 2 * m2 * v2 * Math.cos(theta2 - phi)) / (m1 + m2)
-    ) * Math.sin(phi) + v1 * Math.sin(theta1 - phi) * Math.sin(phi + Math.PI / 2);
-    const v2x = (
-        ((v2 * Math.cos(theta2 - phi)) * (m2 - m1) + 2 * m1 * v1 * Math.cos(theta1 - phi)) / (m1 + m2)
-    ) * Math.cos(phi) + v2 * Math.sin(theta2 - phi) * Math.cos(phi + Math.PI / 2);
-    const v2y = (
-        ((v2 * Math.cos(theta2 - phi)) * (m2 - m1) + 2 * m1 * v1 * Math.cos(theta1 - phi)) / (m1 + m2)
-    ) * Math.sin(phi) + v1 * Math.sin(theta2 - phi) * Math.sin(phi + Math.PI / 2);
-
+    const v1x = (((v1 * Math.cos(theta1 - phi)) * (m1 - m2) + 2 * m2 * v2 * Math.cos(theta2 - phi)) / (m1 + m2)) * Math.cos(phi) + v1 * Math.sin(theta1 - phi) * Math.cos(phi + Math.PI / 2);
+    const v1y = (((v2 * Math.cos(theta1 - phi)) * (m1 - m2) + 2 * m2 * v2 * Math.cos(theta2 - phi)) / (m1 + m2)) * Math.sin(phi) + v1 * Math.sin(theta1 - phi) * Math.sin(phi + Math.PI / 2);
+    const v2x = (((v2 * Math.cos(theta2 - phi)) * (m2 - m1) + 2 * m1 * v1 * Math.cos(theta1 - phi)) / (m1 + m2)) * Math.cos(phi) + v2 * Math.sin(theta2 - phi) * Math.cos(phi + Math.PI / 2);
+    const v2y = (((v2 * Math.cos(theta2 - phi)) * (m2 - m1) + 2 * m1 * v1 * Math.cos(theta1 - phi)) / (m1 + m2)) * Math.sin(phi) + v1 * Math.sin(theta2 - phi) * Math.sin(phi + Math.PI / 2);
     // assigning the values
     p1.velocityX = v1x;
     p1.velocityY = v1y;
     p2.velocityX = v2x;
     p2.velocityY = v2y;
 }
+//# sourceMappingURL=2dcollisions.js.map
--- src/effects/circles.ts
+++ src/effects/circles.ts
@@ -12,69 +12,100 @@ export interface circleType {
 
 export class circle implements Effect {
     private readonly radius: number;
-    private readonly positionX: number;
-    private readonly positionY: number;
+    // private readonly positionX: number;
+    // private readonly positionY: number;
     private readonly velocityX: number;
     private readonly amount: number;
     private readonly velocityY: number;
-    private readonly mass: number = 0;
+    private readonly mass: number;
     private readonly circles: circleType[] = [];
 
-    constructor({ amount = 10, radius = 40, positionX = 70, positionY = 72, velocityX = 10, velocityY = 10, mass = 20 }:
+    private readonly positionBound: number[];
+
+    constructor({ amount = 10, radius = 50, velocityX = 10, velocityY = 10, mass = 100, positionBound = [10, 50] }:
         {
-            amount?: number, radius?: number, positionX?: number, positionY?: number, velocityX?: number,
-            velocityY?: number, mass?: number
+            amount?: number, radius?: number, velocityX?: number,
+            velocityY?: number, mass?: number, positionBound?: number[]
         } = {}) {
         this.radius = radius;
-        this.positionX = positionX;
+        this.positionBound = positionBound;
+        // this.positionX = Math.floor(Math.random() * 100) + 1;
+        // this.positionY = Math.floor(Math.random() * 100) + 1;
         this.velocityX = velocityX;
         this.amount = amount;
         this.velocityY = velocityY;
-        this.positionY = positionY;
+        this.mass = mass;
 
         let circleObj: circleType;
         for (let i = 0; i < this.amount; i++) {
-            circleObj = {
-                positionX: this.positionX + Math.random() * 100,
-                positionY: this.positionY + Math.random() * 100,
-                velocityX: this.velocityX,
-                velocityY: this.velocityY,
-                radius: this.radius,
-                mass: this.mass
+            if (this.amount === 1) {
+                circleObj = {
+                    positionX: this.positionBound[0],
+                    positionY: this.positionBound[1],
+                    velocityX: this.velocityX,
+                    velocityY: this.velocityY,
+                    radius: this.radius,
+                    mass: this.mass
+                }
+                this.circles.push(circleObj);
             }
-            this.circles.push(circleObj);
+            else {
+                const posX = Math.floor(Math.random() * (this.positionBound[0])) + 1;
+                const posY = Math.floor(Math.random() * (this.positionBound[1])) + 1;
+                circleObj = {
+                    positionX: posX,
+                    positionY: posY,
+                    velocityX: this.velocityX,
+                    velocityY: this.velocityY,
+                    radius: this.radius,
+                    mass: this.mass
+                }
+                this.circles.push(circleObj);
+            }
         }
     }
     update(ctx: CanvasRenderingContext2D, state: EngineState) {
         // bounce of the walls
         for (const c of this.circles) {
-            if (c.positionX + this.radius > state.width || c.positionX - this.radius < 0) {
-                c.velocityX! *= -1;
+            if (c.positionX + this.radius > state.width) {
+                c.velocityX *= -1;
+                c.positionX = state.width - this.radius;
             }
-            if (c.positionY + this.radius > state.height || c.positionY - this.radius < 0) {
-                c.velocityY! *= -1;
+            if (c.positionX - this.radius < 0) {
+                c.velocityX *= -1;
+                c.positionX = this.radius;
+            }
+            if (c.positionY + this.radius > state.height) {
+                c.velocityY *= -1;
+                c.positionY = state.height - this.radius;
+            }
+            if (c.positionY - this.radius < 0) {
+                c.velocityY *= -1;
+                c.positionY = this.radius;
             }
 
             c.positionX += c.velocityX!;
             c.positionY += c.velocityY!;
 
+        }
 
-            // collision rendering
-            for (let j = 0; j < this.circles.length; j++) {
-                for (let i = j + 1; i < this.circles.length; i++) {
-                    const p1 = this.circles[j];
-                    const p2 = this.circles[i];
+        // collision rendering
+        for (let j = 0; j < this.circles.length; j++) {
+            for (let i = j + 1; i < this.circles.length; i++) {
+                const p1 = this.circles[j];
+                const p2 = this.circles[i];
 
-                    const dx = p1.positionX - p2.positionX;
-                    const dy = p2.positionY - p2.positionY;
-                    const distance = Math.hypot(dx, dy);
-                    if (distance < p1.radius + p2.radius) {
-                        resolveCollisions(p1, p2);
-                    }
+                const dx = p1.positionX - p2.positionX;
+                const dy = p1.positionY - p2.positionY;
+
+                const distance = Math.hypot(dx, dy);
+                if (distance < p1.radius + p2.radius) {
+                    resolveCollisions(p1, p2);
                 }
             }
+        }
 
-
+        for (const c of this.circles) {
             // drawing
             ctx.beginPath();
             ctx.arc(c.positionX, c.positionY, this.radius, 0, Math.PI * 2, false);
@@ -82,8 +113,6 @@ export class circle implements Effect {
             ctx.fill();
             ctx.closePath();
         }
-
-
 
     }
 }
